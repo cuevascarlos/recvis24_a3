ebb0d126c0fe0c5fad56b6305e1c1b135e482e5c
cuevascarlos <carlos.cuevas-villarmin@universite-paris-saclay.fr>
2024-11-26 21:31:33 +0100

    Added new models

--- model_factory.py
+++ model_factory.py
@@ -1,24 +1,50 @@
 """Python file to instantite the model and the transform that goes with it."""
 
-from data import data_transforms
-from model import Net
+from data import data_transforms, data_transforms_transformers
+from model import Net, Dinov2, Dinov2Giant, ResNet, MultiEmbeddingsClassifier, FusionClassifier, DoubleModel
+from transformers import AutoImageProcessor
 
 
 class ModelFactory:
-    def __init__(self, model_name: str):
+    def __init__(self, model_name: str, nclasses: int = 500):
         self.model_name = model_name
+        self.nclasses = nclasses
         self.model = self.init_model()
         self.transform = self.init_transform()
 
     def init_model(self):
         if self.model_name == "basic_cnn":
-            return Net()
+            return Net(nclasses=self.nclasses)
+        elif self.model_name == "dino":
+            return Dinov2(nclasses=self.nclasses)
+        elif self.model_name == "dino_giant":
+            return Dinov2Giant(nclasses=self.nclasses)
+        elif self.model_name == "ResNet":
+            return ResNet(nclasses=self.nclasses)
+        elif self.model_name == "multi-embeddings":
+            return MultiEmbeddingsClassifier(visual_dim=768, textual_dim=768, num_classes=self.nclasses)
+        elif self.model_name == "fusion":
+            return FusionClassifier(visual_dim=768, textual_dim=768,fusion_dim=512, num_classes=self.nclasses)
+        elif self.model_name == "double":
+            return DoubleModel(visual_dim=768, textual_dim=768, num_classes=self.nclasses)
         else:
             raise NotImplementedError("Model not implemented")
 
     def init_transform(self):
         if self.model_name == "basic_cnn":
             return data_transforms
+        elif self.model_name == "dino":
+            return data_transforms_transformers
+        elif self.model_name == "dino_giant":
+            return data_transforms_transformers
+        elif self.model_name == "ResNet":
+            return data_transforms_transformers
+        elif self.model_name == "multi-embeddings":
+            pass
+        elif self.model_name == "fusion":
+            pass
+        elif self.model_name == "double":
+            pass
         else:
             raise NotImplementedError("Transform not implemented")
 
