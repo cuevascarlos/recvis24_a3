8a07efa9ef9964b90386487eef8d064f9d62adad
cuevascarlos <carlos.cuevas-villarmin@universite-paris-saclay.fr>
2024-11-26 21:31:51 +0100

    Added new models

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
-nclasses = 500
+from transformers import AutoModel, AutoImageProcessor, Dinov2ForImageClassification, ResNetForImageClassification
+import torch
+#nclasses = 500
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, nclasses=500):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
@@ -20,3 +21,136 @@ class Net(nn.Module):
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         return self.fc2(x)
+
+class Dinov2(nn.Module):
+    def __init__(self, nclasses=500):
+        super(Dinov2, self).__init__()
+        #self.processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+        self.model = Dinov2ForImageClassification.from_pretrained('facebook/dinov2-base', num_labels=nclasses)
+
+        for name, param in self.model.named_parameters():
+            if "classifier" not in name:
+                param.requires_grad = False
+
+    def forward(self, x):
+        return self.model(x).logits
+
+class Dinov2Giant(nn.Module):
+    def __init__(self, nclasses=500):
+        super(Dinov2Giant, self).__init__()
+        #self.processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+        self.model = Dinov2ForImageClassification.from_pretrained('facebook/dinov2-giant', num_labels=nclasses)
+
+        for name, param in self.model.named_parameters():
+            if "classifier" not in name:
+                param.requires_grad = False
+
+    def forward(self, x):
+        return self.model(x).logits
+
+class ResNet(nn.Module):
+    def __init__(self, nclasses=500):
+        super(ResNet, self).__init__()
+        self.model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50", num_labels=nclasses, ignore_mismatched_sizes=True)
+    
+        for name, param in self.model.named_parameters():
+            if "classifier" not in name:
+                param.requires_grad = False
+
+    def forward(self, x):
+        return self.model(x).logits
+
+
+class MultiEmbeddingsClassifier(nn.Module):
+    def __init__(self, visual_dim, textual_dim, num_classes=500, dropout_rate=0.2):
+        super(MultiEmbeddingsClassifier, self).__init__()
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(visual_dim+textual_dim, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, visual_emb, textual_emb):
+        x = torch.cat([visual_emb, textual_emb], dim=-1)
+        x = self.classifier(x)
+        return x
+
+'''
+First attempt and fourth: dropout_rate = 0.2
+Second attempt: dropout_rate = 0.5
+Third attempt: dropout_rate = 0.5 y 3 linear layers
+5: dropout_rate = 0.2 y multiheadattention
+6: dropout_rate = 0.2 y mean text embeddings y 2 linear layers (no hecho)
+7: dropout_rate = 0.2 y mean text embeddings y 3 linear layers
+8: dropout_rate = 0.2 y mean text embeddings y 3 linear layers y mean embed en segunda
+'''
+
+class FusionClassifier(nn.Module):
+    def __init__(self, visual_dim, textual_dim, fusion_dim, num_classes=500, dropout_rate=0.2):
+        super(FusionClassifier, self).__init__()
+        self.visual_proj = nn.Linear(visual_dim, fusion_dim)
+        self.text_proj = nn.Linear(textual_dim, fusion_dim)
+        self.fusion_proj = nn.Linear(2 * fusion_dim, fusion_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.classifier = nn.Linear(fusion_dim,num_classes)
+
+    def forward(self, visual_emb, text_emb):
+        visual_emb_proj = self.visual_proj(visual_emb)
+        text_emb_proj = self.text_proj(text_emb)
+        combined = torch.cat([visual_emb_proj, text_emb_proj], dim=-1)
+        fused = self.fusion_proj(combined)
+        fused = self.relu(fused)
+        fused = self.dropout(fused)
+        fused = self.classifier(fused)
+        return fused
+
+class DoubleModel(nn.Module):
+    def __init__(self, visual_dim, textual_dim, num_classes=500, dropout_rate=0.2):
+        super(DoubleModel, self).__init__()
+        self.num_classes = num_classes
+
+        self.visual_classifier = nn.Sequential(
+            nn.Linear(visual_dim, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(512, num_classes)
+        )
+
+        self.textual_classifier = nn.Sequential(
+            nn.Linear(textual_dim, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(512, num_classes)
+        )
+
+        self.weight_layer = nn.Linear(2, 1, bias=False)
+        nn.init.constant_(self.weight_layer.weight, 0.5)
+    
+    def regularization_loss(self):
+        # weights >= 0
+        positive_penalty = torch.sum(torch.abs(torch.min(self.weight_layer.weight, torch.tensor(0.0, device=self.weight_layer.weight.device))))
+
+        # To sum 1
+        weight_sum = self.weight_layer.weight.sum(dim=1)
+        normalization_penalty = torch.abs(weight_sum - 1).sum()
+        return positive_penalty + normalization_penalty
+
+    def forward(self, visual_emb, textual_emb):
+
+        textual_logits = self.textual_classifier(textual_emb)
+        visual_logits  = self.visual_classifier(visual_emb)
+
+        combined_logits = torch.stack([visual_logits, textual_logits], dim=-1)
+        output_logits = self.weight_layer(combined_logits).squeeze(-1)
+        return output_logits
+
+'''
+1: 10 epochs
+2: 15 epochs
+3: 10 epochs y parametros aprendibles alpha y beta + penalización que sumen 1 0.001
+4 y 6: 10 epochs + parametros aprendibles alpha y beta + penalización que sumen 1 y sean positivos 0.1
+5: 10 epochs + parametros aprendibles alpha y beta + penalización que sumen 1 y sean positivos 0.3
+'''
